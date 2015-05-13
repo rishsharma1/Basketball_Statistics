87258ebe991ebe7896f9216a150856cf01d79c84
rishsharma1996 <rishsharma13@gmail.com>
2015-05-13 17:03:12 +1000

    coloring

--- data.py
+++ data.py
@@ -1,24 +1,28 @@
 import csv
+from collections import defaultdict
+
+SEASON_LENGTH = 81
+MAX_AST_PG = 10
 
 #will get all the data that is in 
 #the csv file, some of this data may
 #be used and may not be.
 def get_all_data(input_file):
 
-	csv_file = open(input_file) 
-	data = csv.DictReader(csv_file)
-	store = {}
+    csv_file = open(input_file) 
+    data = csv.DictReader(csv_file)
+    store = {}
 
-	for row in data:
+    for row in data:
 
-		for key in row.keys():
-			
-			try:
-				store[key].append(row[key])
-			except KeyError:
-				store[key] = [row[key]] 
+        for key in row.keys():
+            
+            try:
+                store[key].append(row[key])
+            except KeyError:
+                store[key] = [row[key]] 
 
-	return store
+    return store
 
 #this is used to get a list of indices of
 #a given attribute and given item of that 
@@ -26,114 +30,146 @@ def get_all_data(input_file):
 #Example: All the indices of where Kobe Bryant
 #occurs, Key = 'Player', Query = 'Kobe Bryant'
 def get_index(data,key,query):
-	pos = []
-	for val in range(len(data[key])):
-		if data[key][val] == query:
-			pos.append(val)
-	return pos
+    pos = []
+    for val in range(len(data[key])):
+        if data[key][val] == query:
+            pos.append(val)
+    return pos
 
 #returns a dictionary of a specific targeted data
 #this can be a specific season's data, or specific
 #player stats etc.
 def get_specific_data(data,att,query):
 
-	specific_info = {}
+    specific_info = {}
 
-	#all the index positions of given player
-	pos = get_index(data,att,query)
+    #all the index positions of given player
+    pos = get_index(data,att,query)
 
-	for i in pos:
+    for i in pos:
 
-		for key in data.keys():
+        for key in data.keys():
 
-			try:
-				specific_info[key].append(data[key][i])
-			except KeyError:
-				specific_info[key] = [data[key][i]]
-	return specific_info
+            try:
+                specific_info[key].append(data[key][i])
+            except KeyError:
+                specific_info[key] = [data[key][i]]
+    return specific_info
 
 #returns data for a player from a specific season
 def get_player_season(data,player,season):
 
-	return get_specific_data(get_specific_data(data,'Player',player),'Season',season)
+    return get_specific_data(get_specific_data(data,'Player',player),'Season',season)
 
 #returns data for a team from a specific season 
 def get_team_season(data,team,season):
-	return get_specific_data(get_specific_data(data,'Tm',team),'Season',season)
+    return get_specific_data(get_specific_data(data,'Tm',team),'Season',season)
 
 #returns unique row and column keys 
 def create_keys(data, row, col):
-	
-	#create lists containing unique keys, row and column
-	unique_row = sorted(list(set(data[row])))
-	unique_col = sorted(list(set(data[col])))
-	
-	return [unique_row, unique_col]	
+    
+    #create lists containing unique keys, row and column
+    unique_row = sorted(list(set(data[row])))
+    unique_col = sorted(list(set(data[col])))
+    
+    return [unique_row, unique_col]    
 
 #sort the rows of pivot table by the number of empty elements in each row
 def sort_pvt_vals(pvt_vals):
-	return sorted(pvt_vals, key=lambda x:len(([num for num in x[1] if num < 0])))
+    return sorted(pvt_vals, key=lambda x:len(([num for num in x[1] if num < 0])))
 
 #create a table of aggregation values
 def get_pvt_vals(data, row, col, val, mode, unique_row, unique_col):
 
-	#create a list containing values of aggregation corresponding to unique_row and unique_col
-	pvt_vals = [] 
-	#used for sorting
-	count = 0
+    #create a list containing values of aggregation corresponding to unique_row and unique_col
+    pvt_vals = [] 
+    #used for sorting
+    count = 0
 
-	for row_item in unique_row:
-		 
-		temp_row = []
+    for row_item in unique_row:
+         
+        temp_row = []
 
-		for col_item in unique_col:
-			
-			#get a list of values corresponding to row and column
-			needed_data = get_specific_data(get_specific_data(data, row, row_item), col, col_item)
-			
-			if(needed_data):
-				
-				#calculate sum of values in the list
-				tot = sum(map(int,needed_data[val]))
-				
-				#sum mode
-				if mode == "SUM":
-					
-					temp_row.append(tot)
-					
-				#average mode
-				else:
-					
-					average = int(tot / float(len(needed_data[val])))
-					temp_row.append(average)
+        for col_item in unique_col:
+            
+            #get a list of values corresponding to row and column
+            needed_data = get_specific_data(get_specific_data(data, row, row_item), col, col_item)
+            
+            if(needed_data):
+                
+                #calculate sum of values in the list
+                tot = sum(map(int,needed_data[val]))
+                
+                #sum mode
+                if mode == "SUM":
+                    
+                    temp_row.append(tot)
+                    
+                #average mode
+                else:
+                    
+                    average = int(tot / float(len(needed_data[val])))
+                    temp_row.append(average)
 
-			#for blank cell 
-			else:
+            #for blank cell 
+            else:
 
-				temp_row.append(-1)
-			
-		pvt_vals.append((count, temp_row))
-		count += 1
-			
-	return sort_pvt_vals(pvt_vals)
+                temp_row.append(-1)
+            
+        pvt_vals.append((count, temp_row))
+        count += 1
+            
+    return sort_pvt_vals(pvt_vals)
 
 #returns string of html coding a pivot table
 def create_table_str(pvt_vals, unique_row):
-	
-	table_str = '<tbody>\n'
+    
+    table_str = '<tbody>\n'
 
-	for index in range(len(pvt_vals)):
+    for index in range(len(pvt_vals)):
 
-		table_str += '<tr>\n<td>%d</td>\n<td>%s</td>\n'%(pvt_vals[index][0], unique_row[pvt_vals[index][0]])
+        table_str += '<tr>\n<td>%d</td>\n<td>%s</td>\n'%(pvt_vals[index][0], unique_row[pvt_vals[index][0]])
 
-		for item in pvt_vals[index][1]:
+        for item in pvt_vals[index][1]:
 
-			if(item >= 0):			
-				table_str += '<td>%d</td>\n'%(item)
-			else:
-				table_str += '<td></td>\n'
+            if(item >= 0):            
+                table_str += '<td>%d</td>\n'%(item)
+            else:
+                table_str += '<td></td>\n'
 
-	table_str += '</tbody>\n'
+    table_str += '</tbody>\n'
   
-	return table_str
+    return table_str
 
+all_data = get_all_data('dataclean.csv')
+
+
+def assists_bin(data):
+
+    curr = 0
+    ast_binned = defaultdict(int)
+
+    while curr < MAX_AST_PG:
+
+        for index in range(len(data['AST'])):
+
+            val = float(data['AST'][index])/SEASON_LENGTH
+
+            if val >= curr and val < curr+1:
+                ast_binned[str(curr)+' to '+str(curr+1)] += 1
+
+        curr += 1
+    for index in range(len(data['AST'])):
+
+        val = float(data['AST'][index])/SEASON_LENGTH
+        if val >= MAX_AST_PG:
+            ast_binned['>=10'] += 1
+
+    return ast_binned
+
+kobe_data = get_specific_data(all_data,'Player','Kobe Bryant')
+
+print assists_bin(kobe_data)
+
+
+        
--- view.py
+++ view.py
@@ -1,11 +1,5 @@
 import math
-
-#coloring codes
-table_color = ['#CC7A00','#E68A00','#FF9900','#FFA319','#FFAD33','#FFB84D',
-                '#FFC266','#FFCC80','#FFD699','#FFE0B2','#FFEBCC','#FFF5E6',
-                '#EBEBFF','#D6D6FF','#C2C2FF','#ADADFF','#9999FF','#8585FF',
-                '#7070FF','#5C5CFF','#4747FF','#3333FF','#2E2EE6','#2929CC'] 
-table_color = table_color[::-1]
+import data
 
 def create_header():
     
@@ -58,6 +52,40 @@ def create_table_header(row, unique_col):
     theader_str += '</tr>\n</thead>\n'
     return theader_str
 
+# takes a sorted list to create a list containing lower and upper threshold values
+# any data outside the thresholds are regarded as outliers
+def get_outlier_thresholds(sList):
+    
+    q1 = 0
+    q2 = 0
+    median = 0
+    mRank = ((len(sList) + 1) / 2.0)       # The median rank
+
+    # calculate median.
+    if mRank % 2.0 != 0:
+        median = (sList[int(math.floor(mRank)) - 1] + sList[int(math.ceil(mRank)) - 1]) / 2.0
+    else:
+        median = sList[int(mRank) - 1]
+    
+    # calculate lower and upper quartiles
+    lRank = (math.floor(mRank) + 1) / 2.0        # The lower quartile rank
+    uRank = len(sList) - lRank + 1        # The upper quartile rank
+    
+    if lRank % 2.0 != 0:
+        q1 = (sList[int(math.floor(lRank)) - 1] + sList[int(math.ceil(lRank)) - 1]) / 2.0
+        q3 = (sList[int(math.floor(uRank)) - 1] + sList[int(math.ceil(uRank)) - 1]) / 2.0
+    else:
+        q1 = sList[int(lRank) - 1]
+        q3 = sList[int(uRank) - 1]
+        
+    #calculate interquartile difference
+    iqd = q3 - q1
+    lThsld = q1 - 1.5 * iqd if q1 - 1.5 * iqd >= 0 else 0
+    uThsld = q3 + 1.5 * iqd
+    
+    #print "%f %f %f %f"%(q1, q3, lThsld, uThsld)
+    return [lThsld, uThsld]
+
 #create css of pivot table
 def create_table_css(pvt_vals, row_len, col_len):
 
@@ -76,9 +104,8 @@ def create_table_css(pvt_vals, row_len, col_len):
     table_css_str += '}\n'
 
     #max and min of sum or average corresponding to unique_row and unique_col
-    maxval = max([max(pvt_vals[i][1]) for i in range(len(pvt_vals))])
-    minval = min([min([val for val in pvt_vals[i][1] if val >= 0]) for i in range(len(pvt_vals))])
-    agg_count = 0
+    table_css_str += '%s'%(sorted([val for i in range(len(pvt_vals)) for val in pvt_vals[i][1] if val >= 0]))
+    minval, maxval = get_outlier_thresholds(sorted([val for i in range(len(pvt_vals)) for val in pvt_vals[i][1] if val >= 0]))
 
     #give colours to each cell of the table
     for row_cnt in range(0, row_len+1):
@@ -95,23 +122,24 @@ def create_table_css(pvt_vals, row_len, col_len):
                 table_css_str += '}\n'                 
                 continue
             
-            if(pvt_vals[row_cnt-1][1][col_cnt-3] > 0):            
+            pvt_val = pvt_vals[row_cnt-1][1][col_cnt-3]
+            if(pvt_val > 0):              
 
-                if True:    
-
-                    #pick a color by calculating the relative position of the cell value using hsl
-                    rel_pos = int(abs(math.ceil((((pvt_vals[row_cnt-1][1][col_cnt-3] - minval) / float(maxval - minval)) * 100))))
-                    table_css_str += '/* rel_pos=%d */\n'%(rel_pos)
-                    if rel_pos >= 51:
-                        colour = 'hsla(%d,%d%%,%d%%,%f)'%(235, 100, 98-int((rel_pos-51)*(45/float(49))), 0.8)    
+                # adjust outliers to the threshold values
+                if pvt_val < minval:
+                    pvt_val = minval
+                if pvt_val > maxval:
+                    pvt_val = maxval
                     
-                    else:
-                        colour = 'hsla(%d,%d%%,%d%%,%f)'%(0, 100, 50+int(rel_pos*(48/float(50))), 0.8)    
+                #pick a color by calculating the relative position of the cell value using hsl
+                rel_pos = int(abs(math.ceil((((pvt_val - minval) / float(maxval - minval)) * 100))))
+                table_css_str += '/* min=%f, max = %f, rel_pos=%d */\n'%(minval, maxval, rel_pos)
+                if rel_pos >= 51:
+                    colour = 'hsla(%d,%d%%,%d%%,%f)'%(235, 100, 98-int((rel_pos-51)*(45/float(49))), 0.8)    
+                    
+                else:
+                    colour = 'hsla(%d,%d%%,%d%%,%f)'%(0, 100, 50+int(rel_pos*(48/float(50))), 0.8)    
     
-                if False:    
-
-                    #pick a color by calculating the relative position of the cell value by hardcoding 
-                    colour = table_color[int(abs(math.ceil((((pvt_vals[row_cnt-1][1][col_cnt-3] - minval) / float(maxval - minval)) * 24) - 1)))]
                     
                 table_css_str += 'tr:nth-child(%d) td:nth-child(%d) {\n'%(row_cnt, col_cnt)
                 table_css_str += '    background: %s;\n'%(colour)
@@ -122,8 +150,6 @@ def create_table_css(pvt_vals, row_len, col_len):
                 table_css_str += 'tr:nth-child(%d) td:nth-child(%d) {\n'%(row_cnt, col_cnt)
                 table_css_str += '    background: %s;\n'%('#FFFFFF')
                 table_css_str += '}\n'                 
-
-            agg_count += 1
 
     table_css_str += '</style>\n' 
     
